54515240e875c5d8d5fd17656a0586859245e1cc
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-14 17:23:22 +0100

    added screenshot recording when testing

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -63,6 +63,7 @@ public class Demo {
         );
     }*/
 
+    /*
     @Test
     void archiveANote() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -112,6 +113,97 @@ public class Demo {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("it.feio.android.omninotes.alpha:id/note_title")));
         screenshot();
         Assert.assertEquals(driver.findElement(By.id("it.feio.android.omninotes.alpha:id/note_title")).getText(), "New Note");
+    }*/
+
+    @Test
+    void searchNote() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName" , "Android");
+        caps.setCapability("deviceName" , "Android Emulator");
+        caps.setCapability("app" , System.getProperty("user.dir")+"/apps/OmniNotes-alphaDebug-6.1.0.apk");
+        driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), caps);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        screenshot();
+        MobileElement el1 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        el1.click();
+        screenshot();
+        MobileElement el2 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        el2.click();
+        screenshot();
+        MobileElement el3 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el3.click();
+        screenshot();
+        el3.sendKeys("X");
+        screenshot();
+        MobileElement el4 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
+        el4.click();
+        screenshot();
+        MobileElement el5 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        el5.click();
+        screenshot();
+        MobileElement el6 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        el6.click();
+        screenshot();
+        MobileElement el7 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el7.click();
+        screenshot();
+        MobileElement el8 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el8.sendKeys("Y");
+        screenshot();
+        MobileElement el9 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
+        el9.click();
+        screenshot();
+        MobileElement el10 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_expand_menu_button"));
+        el10.click();
+        screenshot();
+        MobileElement el11 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/fab_note"));
+        el11.click();
+        screenshot();
+        MobileElement el12 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/detail_title"));
+        el12.click();
+        screenshot();
+        el12.sendKeys("Z");
+        screenshot();
+        MobileElement el13 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("drawer open"));
+        el13.click();
+        screenshot();
+        MobileElement el14 = (MobileElement) driver
+                .findElement(MobileBy.AccessibilityId("Search"));
+        el14.click();
+        screenshot();
+        MobileElement el15 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/search_plate"));
+        el15.click();
+        screenshot();
+        MobileElement el16 = (MobileElement) driver
+                .findElement(By.id("it.feio.android.omninotes.alpha:id/search_src_text"));
+        el16.sendKeys("X");
+        screenshot();
+
+        ((AndroidDriver)driver).pressKey(new KeyEvent(AndroidKey.ENTER));
+
+        screenshot();
+
+        Assert.assertEquals(
+                driver.findElement(
+                        By.id("it.feio.android.omninotes.alpha:id/note_title")
+                ).getText(),
+                "X"
+        );
     }
 
     @AfterClass
